7bfde3fee82996dd3520129cf8e543d4d3382b18
lsndr <alexander@lsndr.me>
2019-12-13 00:21:15 +0300

    additional improvements

--- build_binary.js
+++ build_binary.js
@@ -1,25 +1,31 @@
 // @ts-nocheck
 const fs = require('fs')
-const cp = require('child_process')
-const { tdlib } = require('./package.json')
+const {spawn, execSync} = require('child_process');
+const {tdlib} = require('./package.json')
 
 console.log('Downloading tdlib...')
 
-cp.execSync('rm -fR td && git clone https://github.com/tdlib/td.git && cd td && git checkout ' + tdlib.commit + ' && cd ../', {stdio: 'ignore'})
+execSync('git clone https://github.com/tdlib/td.git && cd td && git checkout ' + tdlib.commit + ' && cd ../', {stdio: 'ignore'})
 
 console.log('Launching compiler, please wait...')
 
-let cmdline = 'cmake-js compile -C'
-if (tdlib.debug) cmdline += ' -D'
+let cmd = 'cmake-js'
+let cmdOptions = ['compile', '-C']
+if (tdlib.debug) cmdOptions.push('-D')
 
-cp.execSync(cmdline)
+let cmakeJs = spawn(cmd, cmdOptions)
+cmakeJs.stdout.on('data', (data) => console.log(data.toString()))
+cmakeJs.stderr.on('data', (data) => console.error(data.toString()))
+cmakeJs.on('exit', function (code) {
+    if (code === 0) {
+        console.log('Moving artifact into correct location...')
 
-console.log('Moving artifact into correct location...')
+        fs.renameSync(`./build/${tdlib.debug ? 'Debug' : 'Release'}/tdlib.node`, './tdlib.node')
+    }
 
-fs.renameSync(`./build/${tdlib.debug ? 'Debug' : 'Release'}/tdlib.node`, './tdlib.node')
+    console.log('Cleaning up...')
 
-console.log('Cleaning up...')
+    execSync('rm -fR build && rm -fR td', {stdio: 'ignore'})
 
-cp.execSync('rm -fR build && rm -fR td', {stdio: 'ignore'})
-
-process.exit(0)
+    process.exit(0)
+})
