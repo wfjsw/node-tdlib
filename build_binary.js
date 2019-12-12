01013b11b344aa28b6f0f01e6b1288680775f075
lsndr <alexander@lsndr.me>
2019-12-12 23:18:05 +0300

    building process optimisation & fixes

--- build_binary.js
+++ build_binary.js
@@ -3,6 +3,10 @@ const fs = require('fs')
 const cp = require('child_process')
 const { tdlib } = require('./package.json')
 
+console.log('Downloading tdlib...')
+
+cp.execSync('rm -fR td && git clone https://github.com/tdlib/td.git && cd td && git checkout ' + tdlib.commit + ' && cd ../', {stdio: 'ignore'})
+
 console.log('Launching compiler, please wait...')
 
 let cmdline = 'cmake-js compile -C'
@@ -13,5 +17,9 @@ cp.execSync(cmdline)
 console.log('Moving artifact into correct location...')
 
 fs.renameSync(`./build/${tdlib.debug ? 'Debug' : 'Release'}/tdlib.node`, './tdlib.node')
+
+console.log('Cleaning up...')
+
+cp.execSync('rm -fR build && rm -fR td', {stdio: 'ignore'})
 
 process.exit(0)
