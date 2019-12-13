29622afbd8ac85cd21c506754e70fe7138bff868
Alexander <alexander@lsndr.me>
2019-12-13 06:41:33 +0300

    Building process optimisation & fixes (#4)
    * fix
    
    * fix
    
    * building process optimisation & fixes
    
    * additional improvements
    
    * following lint rules
    
    * made in accrodance with latest changes

--- build_binary.js
+++ build_binary.js
@@ -1,17 +1,31 @@
 // @ts-nocheck
 const fs = require('fs')
-const cp = require('child_process')
+const { spawn, execSync } = require('child_process')
 const { tdlib } = require('./package.json')
+
+console.log('Downloading tdlib...')
+
+execSync('git clone https://github.com/tdlib/td.git && cd td && git checkout ' + tdlib.commit + ' && cd ../', {stdio: 'ignore'})
 
 console.log('Launching compiler, please wait...')
 
-let cmdline = 'cmake-js compile -C'
-if (tdlib.debug) cmdline += ' -D'
+let cmd = 'cmake-js'
+let cmdOptions = ['compile', '-C']
+if (tdlib.debug) cmdOptions.push('-D')
 
-cp.execSync(cmdline)
+let cmakeJs = spawn(cmd, cmdOptions)
+cmakeJs.stdout.on('data', (data) => console.log(data.toString()))
+cmakeJs.stderr.on('data', (data) => console.error(data.toString()))
+cmakeJs.on('exit', function (code) {
+    if (code === 0) {
+        console.log('Moving artifact into correct location...')
 
-console.log('Moving artifact into correct location...')
+        fs.renameSync(`./build/${tdlib.debug ? 'Debug' : 'Release'}/tdlib.node`, './tdlib.node')
+    }
 
-fs.renameSync(`./build/${tdlib.debug ? 'Debug' : 'Release'}/tdlib.node`, './tdlib.node')
+    console.log('Cleaning up...')
 
-process.exit(0)
+    execSync('rm -fR build && rm -fR td', {stdio: 'ignore'})
+
+    process.exit(0)
+})
